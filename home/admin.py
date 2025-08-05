42fac55b9ac2bb0e421b6fd28e2106477a5f4deb
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-05 23:13:20 +0530

    22754

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin
+from django.db import models
+class Restaurants(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+ RESTAURANT_NAME = 'Your Restaurant Name'
+ from django.shortcuts import render
+ from .models import Restaurant
+def homepage(request):
+    try:
+        restaurant = Restaurant.objects.first()
+        return render(request, 'homepage.html', {'restaurant_name': restaurant.name})
+     except Restaurant.DoesNotExist:
+        return render(request, 'homepage.html', {'restaurant_name':'Default Name'}) 
+from django.shortcuts import render
+from django.conf import settings
+def homepage(request, 'homepage.html', {'restaurant_name: settings.RESTAURANT_NAME'})
+from django .urls import path
+from . import views
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    ]
 
-# Register your models here.
