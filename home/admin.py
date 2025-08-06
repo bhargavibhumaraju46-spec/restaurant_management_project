79056d09cd7d5b877e155bd461bf48bea44a3d43
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-06 22:50:07 +0530

    22756

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-def about_us(request):
-    return render(request, 'about_us.html')
-    from django.urls import path
-    from .import views
-url patterns = [path('about-us/', views.about_us, name='about_us'),
-  ]    
+def menu_view(request):
+    menu_items = [
+        {'name': 'Item 1', 'price':10.99},
+        {'name': 'Item 2', 'price': 8.9},
+        {'name': 'Item 3', 'price':7.9},
+    ]
+       return render(request,'menu.html', {'menu_items': menu_items})
+from django.urls import path
+from .import views
+urlpatterns = [
+    path('menu/', views.menu_view, name='menu_view'),
+]       
 
 
