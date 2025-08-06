189cdbe68a8f62ce26e2d8c06feb5d781aa11a33
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-06 23:08:47 +0530

    22758

--- home/admin.py
+++ home/admin.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-RESTAURANT_PHONE_NUMBER = '+91-1234567890'
-from django.db import models
-class Restaurant(models.Model):
-    phone_number = models.CharField(max_length=20)
-    from django.shortcuts import render
-    from django.conf import settings
-def homepage(request):
-    return render(request, 'homepage.html', {'phone_number': settings.RESTAURANT_PHONE_NUMBER})
-from django.shortcuts import render
-from .models import Restaurant
-def homepage(request):
-    restaurant = Restaurant.objects.first()
-    return render(request, 'homepage.html', {'phone_number': restaurant.phone_number})    
+def contact_us(request):
+    return request, 'contact_us.html')
+from django.urls import path
+fron .import views
+urlpatterns = [path('contact-us/', views.contact_us, name='contact_us'),
+]
