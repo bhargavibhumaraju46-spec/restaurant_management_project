cf8dd4004a6b5c2c607f30c00990e1744ce3609f
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-09 00:00:39 +0530

    24372

--- home/admin.py
+++ home/admin.py
@@ -1,24 +1,7 @@
-rom django.db import models
-class edback(models.Model):
-    comment = model.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-  from django import forms
-  from .models importFeedback
-  class FeedbackForm(forms.ModelForm):
-    class Meta:
-        model = Feedback
-        Fields = ['comment'] 
- from django.shortcuts import render,redirect
- from. forms import FeedbackForm
- def feedback_view(request):
-    if request.method == 'POST'
-    form  =FeedbackForm(request.POST)
-    if form.is_valid():
-        form.save() 
-        return redirect('success')
-        else:
-            form = FeedbackForm()
-            return render(request, 'feedback.html', {'form': form})    
+<!-- templates/base.html -->
+<footer>
+    <p>&copy; 2025 Restaurant Name. All rightsreserved.</p>
+</footer>      
 
 
 
