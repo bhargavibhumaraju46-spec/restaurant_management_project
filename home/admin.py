24876780d2cc842378dc4804b6ec7f94b11fd42d
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-09 00:08:04 +0530

    24371

--- home/admin.py
+++ home/admin.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
-from django.db import DatabaseError
-def my_view(request):
-    try:
-        data = MyModel.objects.all()
-        return render(request, 'template.html', {'data': data})
-    except DatabaseError as e:
-        return render(request, 'error.html', {'error': str(e)})       
+<!-- templates/homepage.html -->
+<form>
+   <input type="text" placeholder="search menu items...">
+   <button type="submit">Search</button>
+</form>         
 
 
 
