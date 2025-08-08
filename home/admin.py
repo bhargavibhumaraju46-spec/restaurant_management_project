829397902df4d5101b38ca4a8569e29f1eedddd5
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-08 23:58:13 +0530

    24373

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,24 @@
-<!-- templates/base.html -->
-<footer>
-     <p>Opening Hours: Mon-Fri: am-9pm, Sat-Sun: 10am_10pm</p>
-<footer>
-
+rom django.db import models
+class edback(models.Model):
+    comment = model.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+  from django import forms
+  from .models importFeedback
+  class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        Fields = ['comment'] 
+ from django.shortcuts import render,redirect
+ from. forms import FeedbackForm
+ def feedback_view(request):
+    if request.method == 'POST'
+    form  =FeedbackForm(request.POST)
+    if form.is_valid():
+        form.save() 
+        return redirect('success')
+        else:
+            form = FeedbackForm()
+            return render(request, 'feedback.html', {'form': form})    
 
 
 
