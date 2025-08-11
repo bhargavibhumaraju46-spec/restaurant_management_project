e8ad9f9aa05e34a47dd5b2f6a7dcc6942010e2e8
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-11 16:04:38 +0530

    29965

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,21 @@
 from django.db import models
-class RestaurantLocation(models.Model):
-    address = models.CharField(max_length=255)
-    city = models.CharField(max_lenth=100)
-    state = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=20)
+class ContactSubmission(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+from django import forms
+from .models import ContactSubmission
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactSubmission
+        fields = ['name', 'email']
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_view')
+        else:
+            form = ContactForm()
+            return render(request, 'contact.html', {'form': form})            
