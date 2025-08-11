18b160cb0069e1339baf27c5efc577316fcf1767
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-11 15:44:40 +0530

    29959

--- home/admin.py
+++ home/admin.py
@@ -1,12 +1,7 @@
-<!-- templates/homepage.html -->
-<h2>Reataurant Location</h2>
-<p>Address: 123 Main St, Anytown, India</p>
-<iframe
-    width="600"
-    height="450"
-    style="border:0" 
-    loading="lazy"
-    allowfullscreen
-    src="https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY&q=123+St, +Anytown, +India">
-</frame>         
-
+from django.db import models
+class MenuItems(models.Model):
+    name = models.harField(max_length=100)
+    description = models.TextField()
+    price = models.decimalField(max_digits=10, decimal_places=2)
+python manage.py makemigrations
+python manage.py migrate    
