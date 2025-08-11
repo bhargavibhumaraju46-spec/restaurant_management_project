8e5fb1b7e014d39ff510a922a79f5acf0882c4d4
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-11 16:31:09 +0530

    29962

--- home/admin.py
+++ home/admin.py
@@ -1,11 +1,9 @@
-from django.db import models
-class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='menu_images/', blank=True)
 from django.shortcuts import render
-from .models import MenuItem
-def menu_view(request):
-    menu_items = MenuItem.objects.all()
-    return render(request, 'menu.html', {'menu_items': menu_items})    
+from .models import RestaurantLocations
+def homepage_view(request):
+    try:
+        restaurant = RestaurantLocation.objects.first()
+    except RestaurantLocation.DoesNotExist:
+        restaurant = None
+    reurn render(request, 'homepage.html', {'restaurant': restaurant})
+         
