d4e46482d294ff0e55884bb05f56edc6af4af61b
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-11 14:52:36 +0530

    27671

--- home/admin.py
+++ home/admin.py
@@ -1,17 +1,22 @@
-from django.db import models
-from django.contrib.auth.models import User
-from .models import Menu
-class Order(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.CASCADE)
-    order_items = models.ManyToManyField(Menu)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    order_status = models.CharField(max_length=20, choices=[
-        ('pending', 'pending'),
-        ('completed', 'completed'),
-        ('cancelled', 'cancelled')
-    ], default='pending')  
-
-
-
-
-
+from rest_framework.views.import APIView
+from rest_framework.response import response
+from rest_framework import serializer
+menu_items = [
+    {"dish_name": "Pasta Carbonara", "Classic creamy pasta with pancetta", "price": 12.99},
+    {"dish_name": "Margherita Pizza", "description": "Tomato, mozzarella, and basil", "price":10.50},
+]
+class MenuAPIView(APIView):
+    def get(self, request):
+        serializer = MenuItemSerializer(menu_items, many=True)
+        return Response(serializer.data)
+from django.urls import path
+from .views import MenuAPIView
+urlpatterns = [
+    path('menu/', MenuAPIView.as_view(), name='menu_api'),
+]
+from django.contrib import admin
+from django.urls import path, include
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('menu_app.urls')),
+]
