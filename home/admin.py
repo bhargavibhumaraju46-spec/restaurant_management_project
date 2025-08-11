7ee5e9c792417be3da0ddd9783c2f8b0de329045
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-11 16:37:45 +0530

    29961

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,9 @@
+RESTAURANT_NAME = 'My Restaurant'
+from django.db import models
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
 from django.shortcuts import render
-from .models import RestaurantLocations
+from .models import Restaurant
 def homepage_view(request):
-    try:
-        restaurant = RestaurantLocation.objects.first()
-    except RestaurantLocation.DoesNotExist:
-        restaurant = None
-    reurn render(request, 'homepage.html', {'restaurant': restaurant})
-         
+    restaurant = Restaurant.objects.first()
+    return rende(request, 'homepage.html', {'reataurant': restaurant})    
