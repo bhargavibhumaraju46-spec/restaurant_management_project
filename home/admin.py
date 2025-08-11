94fa1b58832ba0768452bef2c0477679ea05cf1d
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-11 13:50:37 +0530

    27669

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,15 @@
-from django. import render
+from django.db import models
+from django.contrib.auth.models import User
 from .models import Menu
-def homepage(request):
-    menu_items = Menu.objects.all()
-    return render(request, 'homepage.html', {'menu_items': menu_items})  
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_items = models.ManyToManyField(Menu)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    order_status = models.CharField(max_length=20, choices=[
+        ('pending', 'pending'),
+        ('completed', 'completed'),
+        ('cancelled', 'cancelled')
+    ], default='pending')  
 
 
 
