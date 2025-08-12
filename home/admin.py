93cfbef678fad2aabdd65295c008b25e086a1ca5
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-12 20:11:55 +0530

    31242

--- home/admin.py
+++ home/admin.py
@@ -1,19 +1,13 @@
-from flask import Flask, session, render_template
+class Restaurant:
+    def__init__(self, phone_number):
+        self.phone_number = phone_number
+restaurant = Restaurant("+91 8465883631")
+from flask import Flask, render_template
 app = Flask(__name__)
-app.secret_key = 'your secret_key'
-@app.before_request
-def make_session_permanent()
-if 'cart' not in session:
-    session['cart'] = []
-    @app.route('/add_to_cart/<item_id>')
-def add_to_cart(item_id):
-    session['cart'].append(item_id)
-    session.modified = True
-    return 'Item added to cart'
+restaurant = Restaurant("+91 8465883631")
 @app.route('/')
 def homepage():
-    total_items_in_cart = len(session['cart'])
-    return render_template('homepage.html', total_items=total_items_in_cart)
+    return render_template('homepage.html', restaurant=restaurant)
 if__name__ == '__main__':
     app.run(debug=True)            
 
