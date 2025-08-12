b4a90e8bc0a64a4f8e17fafe70b49fce984b23df
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-12 15:35:53 +0530

    29960

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,10 @@
-RESTAURANT_NAME = 'My Restaurant'
-from django.db import models
-class Restaurant(models.Model):
-    name = models.CharField(max_length=100)
 from django.shortcuts import render
-from .models import Restaurant
-def homepage_view(request):
-    restaurant = Restaurant.objects.first()
-    return rende(request, 'homepage.html', {'reataurant': restaurant})    
+from .models import MenuItem
+def menu_view(request):
+    menu_items = MenuItem.objects.all()
+    return render(request, 'menu.html', {'menu_items': menu_items})
+from django.urls import path
+from .views import menu_view 
+urlpatterns = [
+    path('menu/', menu_view, name='menu'),
+]      
