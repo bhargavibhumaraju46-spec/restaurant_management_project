51deca48d505b45dddc562e4f48bb50e5ec3246f
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-12 20:03:55 +0530

    31239

--- home/admin.py
+++ home/admin.py
@@ -1,27 +1,19 @@
-from django.db import models
-class Contact(models.Model):
-    email = models.EmailField()
-    message = models.TextField()
-from django import forms
-class ContactForm(forms.Form):
-    email = forms.EmailField(label='Your Email', required=True)
-    message = forms.CharField(label='Message', widget=forms.Textarea, required=True)
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        if not email.endswith('@example.com'):
-            raise forms.ValidationError("Email must end with @example.com") 
-            return email
-from django.shortcuts import render, redirect
-from .forms import Contact
-def contact_view(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            message = form.cleaned_data['message']
-            print(f"Email: {email}, Message: {message}")
-            return redirect('success_url')
-       else:
-        form = ContactForm()
-        return render(request, 'contact.html', {'form': form})
+from flask import Flask, session, render_template
+app = Flask(__name__)
+app.secret_key = 'your secret_key'
+@app.before_request
+def make_session_permanent()
+if 'cart' not in session:
+    session['cart'] = []
+    @app.route('/add_to_cart/<item_id>')
+def add_to_cart(item_id):
+    session['cart'].append(item_id)
+    session.modified = True
+    return 'Item added to cart'
+@app.route('/')
+def homepage():
+    total_items_in_cart = len(session['cart'])
+    return render_template('homepage.html', total_items=total_items_in_cart)
+if__name__ == '__main__':
+    app.run(debug=True)            
 
