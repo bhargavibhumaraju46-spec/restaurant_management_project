b2f1c907775ec41229af7fff092bcdc6e4a1d064
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-12 16:02:24 +0530

    31176

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,5 @@
-from django.db import models
-class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='menu_images/', blank=True)     
+class Restaurant:
+    def __init__(self, name, address, opening_hours):
+        self.name = name
+        self.address = address
+        self.opening_hours = opening_hours    
