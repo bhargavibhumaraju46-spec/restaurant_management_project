ea501b48fe2a6fc256b76299aed0f0bcc0c14f4f
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-12 18:19:40 +0530

    31175

--- home/admin.py
+++ home/admin.py
@@ -1,20 +1,9 @@
-def linear_search(data_list, target_value):
-    """
-    performs a linear search to findthe target_value in the data_list.
-    Args:
-       data_list(list):The list to be searched.
-       target_value: The value to searche for.
-      Returns:
-         int: The index of the target_value if found, otherwise -1.
-         """
-         for index, element in enumerate(data_list):
-            if elenent == target_value:
-                return index
-                retutn -1
-  my list = [10, 20, 30, 40, 60]
-  search_target_1 = 15
-  result_1 = linear_search(my_list, search_target_1)
-  if result_1 != -1:
-    print(f" '{search_target_1}' found at index: {result_1}")
-    else:
-        print(f"'{search_target_1}' not found in the list.")
+from PIL import Image
+image_path = 'your_image.jpg'
+try:
+    img = Image.open(image_path)
+    img.show()
+except FileNotFoundError:
+    print(f"Error: Image file not found at '{image_path}'")
+except Exception as e:
+    print(f"An error occured: {e}")        
