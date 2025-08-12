726deaae3f9828eac357465644fdbf005103f4d8
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-12 16:16:11 +0530

    31177

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,7 @@
-class Restaurant:
-    def __init__(self, name, address, opening_hours):
-        self.name = name
-        self.address = address
-        self.opening_hours = opening_hours    
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    address_line1 = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state_province = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20) 
+    country = models.CharField(max_length=100) 
