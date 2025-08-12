04699e1f1de3309724d06f4ef1f90f3e89e7ffcb
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-12 16:51:49 +0530

    31174

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,26 @@
-class Restaurant(models.Model):
-    name = models.CharField(max_length=200)
-    address_line1 = models.CharField(max_length=255)
-    city = models.CharField(max_length=100)
-    state_province = models.CharField(max_length=100)
-    postal_code = models.CharField(max_length=20) 
-    country = models.CharField(max_length=100) 
+from django import forms
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
+from django.core.mail impor send_mail
+from django.http import HttpResponseRedirect
+from .forms import ContactForm
+def contact_view(request):
+    if request.method == 'POST'
+    form = ContactForm(request.POST)
+    if form.is_valid():
+        name = form.cleaned_data['name']
+        email = form.cleaned_data['email']
+        message  = form.cleaned_data['message']
+        send_mail(
+            f"Message from {name}",
+            message,
+            email,
+            ['restaurant_email@example.com'],
+            fail_silently=False,
+        )
+        return HttpResponseRedirect('/thanks')
+    else:
+        form = ContactForm()
+      return render(request, 'contact.html', {'form':form})      
