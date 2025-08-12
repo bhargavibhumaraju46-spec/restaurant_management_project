d3ff7ca5e8b6c9bc3a3798eaebdbbf745aa3f10d
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-12 18:59:08 +0530

    31241

--- home/admin.py
+++ home/admin.py
@@ -1,11 +1,16 @@
+from django.db import models
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='restaurant_logos/')
+from django.shortcuts import render
+from .models import Restaurant
+def homepage(request):
+    restaurant = Reataurant.objects.first()
+    return render(request, 'homepage.html',{'restaurant':restaurant})
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.routa('/about')
-def about():
-    restaurant_info = {
-        'history': 'stablished in 2020, our restautrant brings authentic flavors to India.',
-        'mission': 'To deliver exceptional dining experiences with a blend of tradition and innovation.'
-    }      
-    return render_template('about.html', info=restaurant_info)
-    if __name__ == '__main__':
-        app.run(debug=True)
+@app.route('/')
+def homepage():
+    logo_path = 'path_to_logo.jpg'
+    return render_template('homepage.html', logo_path=logo_path)
+<img src="{{ url_for('static', filename=logo_path) }}" alt="Restaurant Logo">    
