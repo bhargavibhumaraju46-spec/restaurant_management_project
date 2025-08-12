87dcbb4fe2521df8b9d586f11b1e2d2e8da23fdf
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-12 15:39:51 +0530

    29963

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,6 @@
-from django.shortcuts import render
-from .models import MenuItem
-def menu_view(request):
-    menu_items = MenuItem.objects.all()
-    return render(request, 'menu.html', {'menu_items': menu_items})
-from django.urls import path
-from .views import menu_view 
-urlpatterns = [
-    path('menu/', menu_view, name='menu'),
-]      
+from django.db import models
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/', blank=True)     
