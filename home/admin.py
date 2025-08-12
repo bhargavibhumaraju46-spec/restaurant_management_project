2be7c842b46122c65500385fdb930ddb2249b3c8
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-12 18:46:46 +0530

    31240

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,11 @@
-from PIL import Image
-image_path = 'your_image.jpg'
-try:
-    img = Image.open(image_path)
-    img.show()
-except FileNotFoundError:
-    print(f"Error: Image file not found at '{image_path}'")
-except Exception as e:
-    print(f"An error occured: {e}")        
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.routa('/about')
+def about():
+    restaurant_info = {
+        'history': 'stablished in 2020, our restautrant brings authentic flavors to India.',
+        'mission': 'To deliver exceptional dining experiences with a blend of tradition and innovation.'
+    }      
+    return render_template('about.html', info=restaurant_info)
+    if __name__ == '__main__':
+        app.run(debug=True)
