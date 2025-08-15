278d186c586213515b0e9d5c5efd78d4480f2afe
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-15 20:29:58 +0530

    31744

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,9 @@
-from django.contrib.auth import view as auth_views
-from django.urls import path, include
-urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
-]
+from flask import render_template, request
+@app.route('/products/category')
+def categry_page():
+    breadcrumbs = [
+        {'name': 'home', 'url':'/'},
+        {'name': 'products', 'url':'/products'},
+        {'name': 'category', 'url':'request.path'}
+    ]
+    return render_template('category.html', breadcrumbs=breadcrumbs)
