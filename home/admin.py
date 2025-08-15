d0e0521bdfdc8c49846e7c3dc3280f78d62135c4
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-15 20:16:30 +0530

    31741

--- home/admin.py
+++ home/admin.py
@@ -1,19 +1,10 @@
-privacy_policy_text = """
-"""
-from flask importbFlask, render_template_string
-from privacy_policy import privacy_policy_text
+from flask import Flask, render_template
 app = Flask(__name__)
-@app.route('/privacy-policy')
-def privacy_policy():
-    return render_template_string('''
-    <h1> privacy policy</h1>
-    <p>{{ policy_text|safe }}</p>
-    ''',policy_text=privacy_policy_text)
-    @app route('/')
-    def home():
-        return render_template_string('''
-        <h1>homepage</h1>
-        <footer><a href="/privacy-policy">privacypolicy</a></footer>
-        ''')
- if __name__ == '__main__':
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/order_placement')
+def order_placement():
+    return "this is the order placement page!"
+if __name__ == '__main__':
     app.run(debug=True)
