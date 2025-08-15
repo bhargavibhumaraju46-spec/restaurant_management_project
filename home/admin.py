95b848ccaea09c897a9af62201319af659c66871
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-15 20:24:23 +0530

    31742

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,5 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return render_template('index.html')
-@app.route('/order_placement')
-def order_placement():
-    return "this is the order placement page!"
-if __name__ == '__main__':
-    app.run(debug=True)
+from django.contrib.auth import view as auth_views
+from django.urls import path, include
+urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
