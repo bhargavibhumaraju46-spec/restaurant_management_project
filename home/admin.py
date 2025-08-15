34cfbd2606e12dbd2259475e81cc8e638ff879e7
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-15 19:57:50 +0530

    31745

--- home/admin.py
+++ home/admin.py
@@ -1,22 +1,19 @@
-from flask import Flask, render_template
+privacy_policy_text = """
+"""
+from flask importbFlask, render_template_string
+from privacy_policy import privacy_policy_text
 app = Flask(__name__)
-faqs = [] 
-    {"question": "what is this website about?", "answer": "this websiteprovides information and reasources on various topics."},
-    {"question": "how can i contact support?", "answer": "you can contact support via email at support@example.com."},
- ]
-@app.route('/')
-def homepage():
-    return render_template('homepage.html')
-@app.route('/faq')
-def faq():
-    return render_template('faq.html', faqs=faqs)
-if __name__ == '__main__':
+@app.route('/privacy-policy')
+def privacy_policy():
+    return render_template_string('''
+    <h1> privacy policy</h1>
+    <p>{{ policy_text|safe }}</p>
+    ''',policy_text=privacy_policy_text)
+    @app route('/')
+    def home():
+        return render_template_string('''
+        <h1>homepage</h1>
+        <footer><a href="/privacy-policy">privacypolicy</a></footer>
+        ''')
+ if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-    
-
-         
