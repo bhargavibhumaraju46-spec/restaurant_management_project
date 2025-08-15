d5c61ec5853272a679cce73dd87c1d1fa0633507
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-15 20:35:58 +0530

    31743

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,8 @@
-from flask import render_template, request
-@app.route('/products/category')
-def categry_page():
-    breadcrumbs = [
-        {'name': 'home', 'url':'/'},
-        {'name': 'products', 'url':'/products'},
-        {'name': 'category', 'url':'request.path'}
-    ]
-    return render_template('category.html', breadcrumbs=breadcrumbs)
+from flask import  Flask, render_template, request
+app = Flask(__name__)
+@app.route('/order_confirmation')
+def order_confirmation():
+    order_number = request.args.get('order_number', 'N/A')
+    return render_template('confirmation.html', order_number=order_number)
+if __name__ == '__main__':
+    app.run(debug=True)    
