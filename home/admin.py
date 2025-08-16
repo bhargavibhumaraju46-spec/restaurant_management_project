5dfc0c07a1b501af8508309a12dca4d85c26c96a
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-16 20:37:38 +0530

    34148

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,8 @@
-from flask import  Flask, render_template, request
+from flask import  Flask, render_template
 app = Flask(__name__)
-@app.route('/order_confirmation')
-def order_confirmation():
-    order_number = request.args.get('order_number', 'N/A')
-    return render_template('confirmation.html', order_number=order_number)
+@app.route('/')
+def restaurant_homepage():
+    background_image = 'path/to/background.jpg', 'path/to/food2.jpg']
+    return render_template('homepage.html, background_image=background_image, food_images=food_images') 
 if __name__ == '__main__':
     app.run(debug=True)    
