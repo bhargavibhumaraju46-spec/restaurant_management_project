d701c0c84d1c2d28ba6cac96f06866dfa979d684
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-18 23:14:32 +0530

    34146

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,21 @@
-from flask import  Flask, render_template
+from flask import Flask, request,jsonify
+from werkjeug.utils import secure_filename
+import os
 app = Flask(__name__)
-@app.route('/')
-def restaurant_homepage():
-    background_image = 'path/to/background.jpg', 'path/to/food2.jpg']
-    return render_template('homepage.html, background_image=background_image, food_images=food_images') 
-if __name__ == '__main__':
-    app.run(debug=True)    
+UPLOAD_FOLDER = 'static/images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDERALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image part'}), 400
+        file = request.files['image']
+        if file.filename == '':
+            return jsonify({'error': 'No selected file'}), 400 
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return jsonify({'message': 'Image uploaded successfully', 'filename': filename}), 200
+   if __name__ == '__main__':
+    app.run(debug=True)                 
