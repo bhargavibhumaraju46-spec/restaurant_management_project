a59241ad1eafba24e32c274f612e44ab930c497a
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-20 20:52:26 +0530

    35564

--- home/admin.py
+++ home/admin.py
@@ -1,17 +1,10 @@
-class Form(forms.Form):
-    name = forms.CharField(max_length=100)
-    message = formsCharField(widget=forms.Textarea)
-    email = forms.EmailField(label='Your email')
-    def __init__(self, *args, **kwargs):
-        super(ContactForm, self).__init__(*args, **kwargs)
-from django.shortcuts import render
-from .forms import ContactForm
-def contact_view(request):
-    if request.method == 'POST':
-        form =  ContactForm(request.POST)
-    if form.is_valid():
-      email = form.cleaned_data['email']
-      return render(request, 'contact/success.html')
-     else:
-        form =CharField()
-      return render(request, 'contact.html', {'form': form})                 
+from flask import Flask, render_template, redirect, url_for
+app = Flask(__name__)
+@app.route('/contact', methods=['POST'])
+def handle_contact_form():
+    return redirect(url_for('thank_you'))
+@app.route('/thank-you')
+def thank_you():
+    return render_template('thank_you.html') 
+if __name__ == '__main__':
+    app.run(debug=True)                      
