5239d5a240a4201ee99b8447161fcfb71260d9e1
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-20 20:17:34 +0530

    35566

--- home/admin.py
+++ home/admin.py
@@ -1,28 +1,7 @@
-from django.db import models
-class RestaurantSettings{models.Model):
-    opening_hours = models.TextField()
-    def __str__(self):
-        return "Restaurant Opening Hours"
-        RESTAURANT_OPENING_HOURS = {
-            'Monday': '10:00 AM -10:00 PM',
-            'Tuesday': '10:00 AM -10:00 PM',
-            'Wednesday': '10:00 AM -10:00 PM',
-            'Thursday': '11:00 AM - 11:00 PM',
-            'Friday': '9:00 AM - 9:00 PM',
-            'Saturday': '8:00 AM - 9:00 PM',
-            'Sunday': '9:00 AM - 9:00 PM',
-        }
-         from  django.shortcuts import render
-         from .models import RestaurantSettings
-         def homepage(request):
-            Restaurant_hours = RestaurantSettings.objects.first()
-            return render(request, 'homepage.html', {'restaurant_hours': restaurant_hours})
-     {% if restaurant_hours %}
-        <h2>OPENING_HOURS</h2>
-        <!-- If using a model -->
-        <p>{{ restaurant_hours.opening_hours }}</p>
-        <!-- If using settings -->
-        {% for day, hours in Restaurant_hours.items %}
-            <p>{{ day }}: {{ hours }}</p>
-        {% endfor %}
-    {% endif %}              
+from flask import Flask, _string
+app = Flask(__name__)
+@app.route('/')
+def contact_us():
+    return render_template_string(template)
+if __name__ == '__main__':
+    app.run(debug=True)    
