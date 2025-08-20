bb2784a9d84e5923670376a7e5ca7a6ea76dd794
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-20 21:34:59 +0530

    36629

--- home/admin.py
+++ home/admin.py
@@ -1,25 +1,14 @@
-from django.db import models
-class Restaurant(models.Model):
-    description = models.TextField()
-from django import forms
-from .models import Restaurant
-class RestaurantForm(forms.ModelForm):
-    class Meta:
-        model = Restaurant
-        fields = ['description']
-from django.shortcuts import render, redirect
-from .forms import RestaurantForm
-def add_description(request):
-    if request.method == 'POST':
-        form = RestaurantForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('about_us')
-        else:
-            form = RestaurantForm()
-         return render(request, 'add_description.html', {'form': form})
-  from django.shortcuts import rende
-  rfrom .models import Restaurant 
-  def about_us(request):
-    description = Restaurant.objects.first()
-    return render(request, 'about_us.html', {'description': description})
+from django.core.paginator import paginator
+form django.shortcuts import render
+from .models import MenuItem
+def menu_view(request):
+    menu_items = MenuItem.objects.all()
+    paginator = paginator(menu_items, 10)
+    page_number =  request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'menu.html', {'page_obj': page_obj})
+    
+    
+    
+    
+    
