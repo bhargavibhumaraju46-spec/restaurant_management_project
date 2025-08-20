366fb68297ab1be85226e0fc970dabd86cf249f4
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-20 20:35:20 +0530

    35565

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,17 @@
-from flask import Flask, _string
-app = Flask(__name__)
-@app.route('/')
-def contact_us():
-    return render_template_string(template)
-if __name__ == '__main__':
-    app.run(debug=True)    
+class Form(forms.Form):
+    name = forms.CharField(max_length=100)
+    message = formsCharField(widget=forms.Textarea)
+    email = forms.EmailField(label='Your email')
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+from django.shortcuts import render
+from .forms import ContactForm
+def contact_view(request):
+    if request.method == 'POST':
+        form =  ContactForm(request.POST)
+    if form.is_valid():
+      email = form.cleaned_data['email']
+      return render(request, 'contact/success.html')
+     else:
+        form =CharField()
+      return render(request, 'contact.html', {'form': form})                 
