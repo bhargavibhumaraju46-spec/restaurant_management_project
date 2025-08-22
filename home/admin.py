45299918861b2adbf2ac418104dd509d3a99fcc9
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-22 21:56:42 +0530

    37925

--- home/admin.py
+++ home/admin.py
@@ -1,15 +1,6 @@
-restaurant_hours = {
-    "Monday": "9:00 AM - 10:00 PM", 
-    "Tuesday": "9:00 AM - 10:00 PM",
-    "Wednesday": "9:00 AM - 10:00 PM",
-    "Thursday": "10:00 AM - 10:00 PM",
-    "Friday": "10:00 AM - 10:00 PM",
-    "Saturday": "10:00 AM - 10:00 PM",
-    "Sunday": "10:00 AM - 10:00 PM",
- }
- def display_opening_hours(hours):
-    print("--- Restaurant Opening Hours --- ")   
-    for day, time in hours.items():
-        print(f"{day}: {time}")
-    print("----------------")
-display_opening_hours(restaurant_hours)        
+def generate_homepage_breadcrumb():
+    """
+    Generates a simple breadcrumb for the homepage.
+    """
+    return "Home"
+print(generate_homepage_breadcrumb())            
