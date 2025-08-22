31b378482baa13e6c0f95e2f7cf6e8b5f4aa1a77
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-22 21:50:11 +0530

    37924

--- home/admin.py
+++ home/admin.py
@@ -1,15 +1,15 @@
-from bs4 import BeautifulSoap
-html_content = """
-<html>
-<body>
-<img src="example.jpg">
-</body>
-</html>
-"""
-soup = BeautifulSoup(html_content, 'html.parser')
-for img in soup.find_all('img'):
-    if not img.get('alt'):
-        img['alt'] = 'Descriptive alt textfor accesibility'
-print(soup.prettify())
-                 
-    
+restaurant_hours = {
+    "Monday": "9:00 AM - 10:00 PM", 
+    "Tuesday": "9:00 AM - 10:00 PM",
+    "Wednesday": "9:00 AM - 10:00 PM",
+    "Thursday": "10:00 AM - 10:00 PM",
+    "Friday": "10:00 AM - 10:00 PM",
+    "Saturday": "10:00 AM - 10:00 PM",
+    "Sunday": "10:00 AM - 10:00 PM",
+ }
+ def display_opening_hours(hours):
+    print("--- Restaurant Opening Hours --- ")   
+    for day, time in hours.items():
+        print(f"{day}: {time}")
+    print("----------------")
+display_opening_hours(restaurant_hours)        
