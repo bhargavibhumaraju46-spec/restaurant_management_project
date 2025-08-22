561f4088207406d68e0f89413ec22037ff289b35
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-22 21:05:25 +0530

    36627

--- home/admin.py
+++ home/admin.py
@@ -1,17 +1,18 @@
-from django import template
-register = template.Library()
-@register.filter('coming_soon')
-def coming_soon(item):
-    if not item.available:
-        return "Coming Soon"
-     return item.name
-{% load menu_filters %}
-{% for item in menu_items %}
-   {{ item|coming_soon }} 
-{% endfor %}     
-
-    
-    
-    
-    
+from django.db import models
+class Chef(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.TextField()
+    image = models.ImageField(upload_to='chefs/')
+    def __str__(self):
+        return self.name
+from django.shortcuts import render
+from .models import Chef
+def about_chef(request):
+    chef = Chef.objects.first()
+    return render(request, 'about_chef.html', {'chef':chef})
+from django.urls import path
+from .views import about_chef
+urlpatterns = [
+    path('about-chef/', about_chef, name='about_chef'),
+]            
     
