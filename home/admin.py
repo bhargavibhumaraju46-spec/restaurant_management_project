1accb289f8681a100de26dc51518160c25fdd720
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-22 21:14:50 +0530

    36628

--- home/admin.py
+++ home/admin.py
@@ -1,18 +1,15 @@
-from django.db import models
-class Chef(models.Model):
-    name = models.CharField(max_length=255)
-    bio = models.TextField()
-    image = models.ImageField(upload_to='chefs/')
-    def __str__(self):
-        return self.name
-from django.shortcuts import render
-from .models import Chef
-def about_chef(request):
-    chef = Chef.objects.first()
-    return render(request, 'about_chef.html', {'chef':chef})
-from django.urls import path
-from .views import about_chef
-urlpatterns = [
-    path('about-chef/', about_chef, name='about_chef'),
-]            
+from bs4 import BeautifulSoap
+html_content = """
+<html>
+<body>
+<img src="example.jpg">
+</body>
+</html>
+"""
+soup = BeautifulSoup(html_content, 'html.parser')
+for img in soup.find_all('img'):
+    if not img.get('alt'):
+        img['alt'] = 'Descriptive alt textfor accesibility'
+print(soup.prettify())
+                 
     
