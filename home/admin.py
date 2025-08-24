18443d5203a31ae63f8ef5f478b7c412d131c858
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-25 00:05:52 +0530

    37927

--- home/admin.py
+++ home/admin.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 def about():
     return render_template('about.html')
     if __name__ == '__main__':
-        app.run(debug=True)           
+        app.run(debug=True)         
