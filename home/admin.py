53457234602da4796cdaa0d18ccfd5b78c6802b9
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-26 22:59:23 +0530

    38716

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,13 @@
-restaurant_name = "delicious Bites"
-restaurant_location = "Downtown Kamanur"
-homepage_title = f"{restaurant_name} - Best Restaurant in {restaurant_location} | official site"
-print(homepage_title)
+from bs4 import BeautifulSoup
+html_doc = """
+<html>
+<body>
+<img src="restaurant_logo.png">
+</body>
+</html>
+"""
+soup = BeautifulSoup(html_doc, 'html.parser')
+img_tag = soup.find('img')
+if img_tag:
+    img_tag['alt'] = "delicious Italian restaurant logo with a chef's hat and pasta."
+    print(soup.prettify())
