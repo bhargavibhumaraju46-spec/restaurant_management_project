ab9b6f9cecdc7db69bfe74cabd316ffb2acc3410
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-27 23:09:03 +0530

    38717

--- home/admin.py
+++ home/admin.py
@@ -1,13 +1,5 @@
-from bs4 import BeautifulSoup
-html_doc = """
-<html>
-<body>
-<img src="restaurant_logo.png">
-</body>
-</html>
-"""
-soup = BeautifulSoup(html_doc, 'html.parser')
-img_tag = soup.find('img')
-if img_tag:
-    img_tag['alt'] = "delicious Italian restaurant logo with a chef's hat and pasta."
-    print(soup.prettify())
+CONTACT_EMAIL = "contact@example.com"
+from flask import render_template
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', email=CONTACT_EMAIL)
