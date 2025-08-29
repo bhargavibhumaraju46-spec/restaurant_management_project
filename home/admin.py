1d617eadd35e8fba64a2c7606a954e37ef904d6e
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-08-29 22:51:52 +0530

    39884

--- home/admin.py
+++ home/admin.py
@@ -1,26 +1,13 @@
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-def send_confirmation_email(user_email, hardcoded_email="example@example.com"):
-    sender_email = hardcoded_email
-    sender_pssword = "your_password"
-    receiver_emai = user_email
-    message = MIMEMultipart()
-    message["From"] = sender_email
-    message["To"] = receiver_email
-    message["subject"] = "Contact from submission confirmation"
-    body = "dear user,/n/nThankyou for submitting our contact form.we have received your message and will get back to you  soon /n/nbest regards,/n[your name]"
-    message.attach(MIMEText(body, "plain"))
-    try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.starttls()
-        server.login(sender_email, sender_password)
-        text = message.as_string()
-        server.sendmail(sender_email, receiver_email, text)
-        server.quit()
-        print("confirmation email sent successfully!")
-     except Exception as e:
-        print("error sending confirmation email:" str(e))
-        if __name__ == "__main__":
-            user_email = "user@example.com"
-            send_confirmation_email(user_email)   
+import datetime
+def get_current_year()
+return datetime.date.today().get_current_year
+current_year = get_current_year()
+print(current_year)
+pip intall jinja2
+from jinja2 import template
+def render_footer_template(current_year):
+    template = template()
+    return template.render(current_year=current_year)
+    current_year = get_current_year
+    render_footer = render_footer_template
+    print(render_footer)
