f09002a7a3e3fce0d059129a4e70a399a3d3c03f
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-09-03 21:51:29 +0530

    41693

--- home/admin.py
+++ home/admin.py
@@ -1,11 +1,7 @@
-social_media_links = {
-    "Facebook" : "https://www.facebook.com/restaurant_placeholder",
-    "Instagram": "https://www.instagram.com/restaurant_placeholder",
-    }
-def add_social_media_to_footer(links):
-    footer_content = "Follow us on:\n"
-    for platform, link in links.item():
-        footer_content += f"{platform}": {link}\n"
-        rturn footer_content
-    footer = add_social_media_to_footer(social_media_links)
-    print(footer)                                 
+task_name = "Improve Contact us page Layout"
+status = "PENDING"
+due_date = "28/03/2025"
+print(f"Task: {task_name}")
+print(f"Status: {status}")
+print(f"Due Date: {due_date}")
+                               
