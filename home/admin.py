4c1d482758dedb7e133fb89ba7d0370f167b1a5d
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-09-05 22:10:04 +0530

    42640

--- home/admin.py
+++ home/admin.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template_string
-app = Flask(__name__)
-restaurant_images = [
-    "https://example.com/image1.jpg",
-    "https://example.com/image2.jpg",
-    "https://example.com/image3.jpg",
-]
-template = """
-"""
-@app.route('/gallery')
-def gallery():
-    return render_template_string(template, images=restaurant_images)
-if __name__ == '__main__':
-    app.run(debug=True)
+print("""
+<!-- HTML for a simple search bar -->
+<input type="text" id="searchBar" placeholder="Search">
+<button onclick="displaySearchMessage()">search</button>
+<!-- JavaScript -->
+<script>
+function displaySearchMessage() {
+    alert('Search');
+}
+</script>
+""")
