adce226f38713487bd630ed405968c2d546515f9
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-09-05 21:55:45 +0530

    42639

--- home/admin.py
+++ home/admin.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template_string
 app = Flask(__name__)
-privacy_policy_content = """
+restaurant_images = [
+    "https://example.com/image1.jpg",
+    "https://example.com/image2.jpg",
+    "https://example.com/image3.jpg",
+]
+template = """
 """
-@app.route('/privacy-policy')
-def privacy_policy():
-    return render_template_string(privacy_policy_content)
-    homepage_content = """
-    """
-    @app.route('/')
-    def home():
-        return render_template_string(homepage_content)
-        if __name__ == '__main__':
-            app.run(debug=True)
-                               
+@app.route('/gallery')
+def gallery():
+    return render_template_string(template, images=restaurant_images)
+if __name__ == '__main__':
+    app.run(debug=True)
