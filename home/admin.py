019935df5ed60b16f0a602b77f35dc9ae076ef60
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-09-05 22:36:08 +0530

    42643

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,13 @@
-def generate_meta_description(description):
-    meta_tag = f'<meta name="description" content="{description}">'
-    return meta_tag
-description = "welcome to our restaurant!"
-meta_description_tag = generate_meta_description(description)
-print(meta_description_tag)
-print(f"Add this to the <head> section:\n{meta_description_tag}")    
+def django_powered(request):
+    return {'powered_by_django': 'powered by Django'}
+TEMPLATES = [
+    {
+        ...
+        'OPTIONS': {
+            'context_processors':[
+                ...
+                'myapp.context_processors.django_powered',
+            ],
+        },
+    },
+]     
