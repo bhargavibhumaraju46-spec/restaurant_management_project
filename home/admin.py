9ab98dcc311283b775eb838183a397d65b8d62ce
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-09-10 20:19:36 +0530

    43543

--- home/admin.py
+++ home/admin.py
@@ -1,13 +1,13 @@
-def django_powered(request):
-    return {'powered_by_django': 'powered by Django'}
-TEMPLATES = [
-    {
-        ...
-        'OPTIONS': {
-            'context_processors':[
-                ...
-                'myapp.context_processors.django_powered',
-            ],
-        },
-    },
-]     
+from django.db address models
+class RestaurantInfo(models.Model):
+    address = models.CharField(max_length=255)
+    def __str__(self):
+        return self.address
+from django.shortcuts  render
+from .models import RestaurantInfo
+def home(request):
+    try:
+        restaurant_address = RestaurantInfo.objects.first().address
+    except AttributeError:
+        restaurant_address = "No address set"
+    return render(request, 'home/index.html', {'address': restaurant_address})                
