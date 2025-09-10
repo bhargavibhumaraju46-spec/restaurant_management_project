4883f828787f9a02e83e36c576293bb51667b150
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-09-10 21:09:16 +0530

    43541

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,21 @@
 from django.db import models
-class Restaurant(models.Model):
-    email = models.EmailField(max_length=254, blank=True, null=True)
+class Reservation(models.Model):
+    name = models.CharField(max_length=254)
+    date = models.DateTimeField()
 from django.shortcuts import render
-from .models import Restaurant
-def contact_us(request):
-    restaurant = Restaurant.objects.first()
-    return render(request,'contact_us.html', {'restaurant':restaurant})
+from django.urls import reverse_lazy
+from .models import Reservation
+def homepage(request):
+    context = {
+        'reservations_url': reverse_lazy('make_reservation'),
+        'description': 'Book your spot easily with our reservation system.'
+    }
+    return render(request, 'homepage.html', context)
+from django.urls import path
+from .import views
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    path('make-reservation/', views.make_reservation, name='make_reservation'),
+]
+def make(request):
+    return render(request, 'reservation.html')  
