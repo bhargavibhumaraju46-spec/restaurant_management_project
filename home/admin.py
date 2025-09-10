97096572942aab11c8acf0423af88d0f5ca7882f
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-09-10 20:45:30 +0530

    43539

--- home/admin.py
+++ home/admin.py
@@ -1,19 +1,8 @@
-from django.db import model
-class MenuItem(models.Model):
-    name = models.CharField(max_length=255)   
-    description = models.TextField()
+from django.db import models
+class Restaurant(models.Model):
+    email = models.EmailField(max_length=254, blank=True, null=True)
 from django.shortcuts import render
-from .models import MenuItem
-from django.db.models import Q
-def menu(request):
-    query = request.GET.get('q')
-    if query:
-        menu_items = MenuItem.objects.filter(Q(name__icontains=query))
-    else:
-        menu_items = MenuItem.objects.all()
-        return render(request, 'menu/menu.html', {'menu_item':menu_items})
-from django.urls import path
-from .views import menu
-urlpatterns = [
-    path('',menu, name='menu'),
-]           
+from .models import Restaurant
+def contact_us(request):
+    restaurant = Restaurant.objects.first()
+    return render(request,'contact_us.html', {'restaurant':restaurant})
