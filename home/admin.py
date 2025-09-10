911e8ffec507620aaece1f79d8fe8f5476000b5e
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-09-10 20:39:20 +0530

    43542

--- home/admin.py
+++ home/admin.py
@@ -1,13 +1,19 @@
-from django.db address models
-class RestaurantInfo(models.Model):
-    address = models.CharField(max_length=255)
-    def __str__(self):
-        return self.address
-from django.shortcuts  render
-from .models import RestaurantInfo
-def home(request):
-    try:
-        restaurant_address = RestaurantInfo.objects.first().address
-    except AttributeError:
-        restaurant_address = "No address set"
-    return render(request, 'home/index.html', {'address': restaurant_address})                
+from django.db import model
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)   
+    description = models.TextField()
+from django.shortcuts import render
+from .models import MenuItem
+from django.db.models import Q
+def menu(request):
+    query = request.GET.get('q')
+    if query:
+        menu_items = MenuItem.objects.filter(Q(name__icontains=query))
+    else:
+        menu_items = MenuItem.objects.all()
+        return render(request, 'menu/menu.html', {'menu_item':menu_items})
+from django.urls import path
+from .views import menu
+urlpatterns = [
+    path('',menu, name='menu'),
+]           
