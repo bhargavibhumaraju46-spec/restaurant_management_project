2045a052c46b3f5f1d455876c736cb6fee75ae46
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-09-12 23:19:19 +0530

    43915

--- home/admin.py
+++ home/admin.py
@@ -1,21 +1,17 @@
 from django.db import models
-class RestaurantLocation(models.Model):
+class RestaurantInfo(models.Model):
     address = models.CharField(max_length=255)
-    latitude = models.DecimalField(max_digits=10, decimal_places=7)
-    longitude = models.DecimalField(max_digits=10, decimal_places=7)
-    description = models.TextField(blank=True)
  def __str__(self):
     return self.address 
 from django.shortcuts import render
-from .models import RestaurantLocation.objects.first()
-context = {'location': location}
-return render(request, 'locations.html', context) 
-from django.urls import path
-from .import views
-urlpatterns = [
-    path('locations/', views.locations, name='locations'),
-]     
+from .models import RestaurantInfo
+def homepage(request):
+    try:
+        restaurant_info = RestaurantInfo.objects.get(id=1)
+
+    except RestaurantInfo.DoesNotExist:
+        restaurant_info = RestaurantInfo.objects.create(address="Default Address if needed")    
+return render(request, 'home/homepage.html', {'address': restaurant_info.address}) 
 from django.contrib import admin
-from .models import RestaurantLocation
-admin.site.register(RestaurantLocation
-)
+from .models import RestaurantInfo
+admin.site.register(RestaurantInfo)
