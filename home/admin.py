242d8c57ad8d822699a438532ee33f507ae341f1
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-09-12 23:27:41 +0530

    43916

--- home/admin.py
+++ home/admin.py
@@ -1,17 +1,10 @@
-from django.db import models
-class RestaurantInfo(models.Model):
-    address = models.CharField(max_length=255)
- def __str__(self):
-    return self.address 
+from django.urls import path
+from .import views
+urlpattern = [
+    path('cart/', views.cart_view, name='view_cart'),
+]
 from django.shortcuts import render
-from .models import RestaurantInfo
-def homepage(request):
-    try:
-        restaurant_info = RestaurantInfo.objects.get(id=1)
-
-    except RestaurantInfo.DoesNotExist:
-        restaurant_info = RestaurantInfo.objects.create(address="Default Address if needed")    
-return render(request, 'home/homepage.html', {'address': restaurant_info.address}) 
-from django.contrib import admin
-from .models import RestaurantInfo
-admin.site.register(RestaurantInfo)
+from .models import Cart
+def cart_views(request):
+    cart_items = Cart.objects.filter(User=request.user)
+    return render(request, 'cart.html', {'cart_item': cart_items})
