e8c723ce15bd457815e9def59120dc688e361a87
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-09-16 23:42:50 +0530

    45333

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,20 @@
-import secrets
-import string
-def generate_coupon_code(length=10, existing_codes=None):
-    if existing_codes is None:
-        existing_codes = set()
-    while True:
-        code = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(length))
-        if code not in existing _codes:
-            return code            
+from rest_framework import generics
+from .models import MenuCategory
+from .serializers import MenuCategorySerializer
+from rest_framework import serializers
+from .models import MenuCategory
+class MenuCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuCategory
+        fields = ['name']
+from rest_framework import generics
+from .models import MenuCategory
+from .serializers import MenuCategorySerializer
+class MenuCategoryList(generics.ListAPIView):
+    queryset = MenuCategory.objects.all()
+    serializer_class = MenuCategorySerializer
+from django.urls import path
+from .views import MenuCategoryList
+urlpatterns = [
+    path('menu-categories/', MenuCategoryList.as_view(), name='menu-categories'),
+]                    
