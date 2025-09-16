78cb0dbc680ba6791f8108d4704d08c08d9fad06
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-09-16 23:17:50 +0530

    45329

--- home/admin.py
+++ home/admin.py
@@ -1,17 +1,3 @@
-from django.urls import path
-from .import views
-urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('manu/', views.menu, name='menu'),
-    path('about-us/', views.about_us, name='about_us'),
-    path('contact-us/', views.contact_us, name='contact_us'),        
-    ]
-from django.shortcuts import render
-def homepage(request):
-    return render(request, 'homepage.html')
-def menu(request):
-    return render(request, 'menu.html')
-def about_us(request):
-    return render(request, 'about_us.html')
-def contact_us(request):
-    return render(request, 'contact_us.html')            
+from django.db import models
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)          
