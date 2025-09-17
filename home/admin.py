e1b3dd6a92cfc68fba0f2ba5c2b6de6b58b0c4cd
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-09-17 22:18:30 +0530

    47980

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,35 @@
 from django.db import models
-from .models import OrderStatus
+from django.contrib.auth.models import User
 class Order(models.Model):
-    status = models.ForeignKey(OrderStatus, on_delete=model.SET_NULL, null=True)                   
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+class Item(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+ from rest_framework import serializers
+ from .models import Order, Item
+ class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['name', 'price'] 
+class OrderSerializer(serilaizers.ModelSerializer):
+    items = ItemSerializer(many=True, read_only=True)
+    class Meta:
+        model = Order
+        fields = ['id', 'date', 'total_price', 'items']
+from rest_framework import generics
+from rest_framework.permissins import IsAuthenticated
+from .models import Order
+from .serializers import OrderSerializer  
+class OrderHistoryView(generics.ListAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+from django.urls import path
+from .views import OrderHistoryView
+urlpatterns = [
+    path('order-history/', OrderHistoryView.as_view(), name='order_history'),
+]        
