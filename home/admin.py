a51a4554fb6efece6bbc67cfb689ba368373bfb1
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-09-17 22:37:12 +0530

    47979

--- home/admin.py
+++ home/admin.py
@@ -1,35 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import User
-class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
-class Item(models.Model):
-    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
- from rest_framework import serializers
- from .models import Order, Item
- class ItemSerializer(serializers.ModelSerializer):
+class MenuItem(models.Model):
+    category = models.CharField(max_length=255)
+from rest_framework import serializers
+from .models import MenuItem
+class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Item
-        fields = ['name', 'price'] 
-class OrderSerializer(serilaizers.ModelSerializer):
-    items = ItemSerializer(many=True, read_only=True)
-    class Meta:
-        model = Order
-        fields = ['id', 'date', 'total_price', 'items']
+        model = MenuItem
+        fields = '__all__'
 from rest_framework import generics
-from rest_framework.permissins import IsAuthenticated
-from .models import Order
-from .serializers import OrderSerializer  
-class OrderHistoryView(generics.ListAPIView):
-    serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticated]
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+from rest_framework.response import Response  
+class MenuItemByCategoryView(generics.ListAPIView):
+    serializer_class = MenuItemSerializer
     def get_queryset(self):
-        return Order.objects.filter(user=self.request.user)
-from django.urls import path
-from .views import OrderHistoryView
+        category = self.request.query_params.get('category', None)
+        if category is not None:
+            queryset = MenuItem.objects.filter(category=category)
+            else:
+                queryset = MenuItem.objects.none()
+                return queryset
+from .django.urls import path
+from .views import MenuItemByCategoryView
 urlpatterns = [
-    path('order-history/', OrderHistoryView.as_view(), name='order_history'),
+    path('menu-items/by-category/', MenuItemByCategoryView.as_view(), name='menu-items-by-category'),
 ]        
