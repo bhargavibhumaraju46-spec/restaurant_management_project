835d90887b9d3a382d294306423c7294bddb89aa
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-09-25 22:00:46 +0530

    50868

--- account/urls.py
+++ account/urls.py
@@ -1,35 +1,23 @@
-from django.db import models
-class ContactFormSubmission(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
-    message = models.TextField()
-    def __str__(self):
-        return f"{self.name} - {self.email}"
-from rest_framework import serializers
-from .models import ContactFormSubmission
-class ContactFormSubmissionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ContactFormSubmission
-        fields = ['name', 'email', 'message']
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.generics import CreateAPIView
-from .models import ContactFormSubmission
-from .serializers import ContactFormSubmissionSerializer
-class ContactFormSubmissionView(CreateAPIView):
-    queryset = ContactFormSubmission.objects.all()
-    serializer_class = ContactFormSubmissionSerializer
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-from django.urls import path
-from .views import ContactFormSubmissionView
-urlpattererns = [
-    path('contact/', ContactFormSubmissionView.as_view(), name='contact_form_submission'),
-    ]
-
-
-]
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+de send_email(recipient_email, subject, message_body):
+try:
+    smtp_server = "smtp.gmail.com"
+    port = 587
+    sender_email = "your_email@gmail.com"
+    password = "your_password"
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = recipient_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message_body, 'plain'))
+    server = smtplib.SMTP(smtp_server, port)
+    server.starttls()
+    server.login(sender_email, password)
+    text = msg.as_string()
+    server.sendmail(sender_email, recipient_email, text)
+    server.quit()
+    print("Email sent successfully!")
+    except Exception as e:
+        print(f"Error sending email: {e}")
