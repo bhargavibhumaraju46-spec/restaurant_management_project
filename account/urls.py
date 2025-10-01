bc2bbb099917eb1293f5667ed3988b0c770a1e28
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-10-01 23:08:23 +0530

    52481

--- account/urls.py
+++ account/urls.py
@@ -1,18 +1,12 @@
-from datetime import datetim, time
-def is_restaurant_open():
-    opening_hours = {
-        "Monday": (time(9, 0), time(22, 0)),
-        "Tuesday": (time(9,0), time(22, 0)),
-        "Wednesday": (time(9, 0), time(22, 0)),
-        "Thursday": (time(9, 0)), time(22, 0)),
-        "Friday": (time(9, 0)), time(22, 0)),
-        "Saturday": (time(9, 0)), time(22, 0)),
-        "Sunday": (time(9, 0)), time(22, 0)),
-       }
-       now  = datetime.now()
-       current_day = now.strftime("%A")
-       current_time = now.time()
-       open_time, close_time = opening_hours[current_day]
-       return open_time <= current_time <= close_time
-       from home.utils impport is_restaurant_open
-       print(is_restaurant_open)
+from rest_framework import generics
+from .models import Table
+from .serializers import TableSerializer
+class TableDetailView(generics.RetrieveAPIView):
+    queryset = Table.objects.all()
+    serializer_class = TableSerializer
+    lookup_field = 'pk'
+from django.urls import path
+from .views import TableDetailView
+urlpattern = [
+    path('api/tables/<ink:pk>/', TableDetailView.as_view(), name='table-detail'),
+]    
