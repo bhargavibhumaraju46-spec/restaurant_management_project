932c767848da4dd8556c9019fe7650b51ebb6313
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-10-01 22:59:35 +0530

    52480

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,18 @@
-from datetime import date
-from django.db.models import Sum
-from .models import Order
-def get_daily_sales_total(date: date) -> float:
-    total = Order.objects.filter(created_at_date=date).aggregate(total_sum=Sum('toal_price'))['total_sum'] or 0
-    return total    
+from datetime import datetim, time
+def is_restaurant_open():
+    opening_hours = {
+        "Monday": (time(9, 0), time(22, 0)),
+        "Tuesday": (time(9,0), time(22, 0)),
+        "Wednesday": (time(9, 0), time(22, 0)),
+        "Thursday": (time(9, 0)), time(22, 0)),
+        "Friday": (time(9, 0)), time(22, 0)),
+        "Saturday": (time(9, 0)), time(22, 0)),
+        "Sunday": (time(9, 0)), time(22, 0)),
+       }
+       now  = datetime.now()
+       current_day = now.strftime("%A")
+       current_time = now.time()
+       open_time, close_time = opening_hours[current_day]
+       return open_time <= current_time <= close_time
+       from home.utils impport is_restaurant_open
+       print(is_restaurant_open)
