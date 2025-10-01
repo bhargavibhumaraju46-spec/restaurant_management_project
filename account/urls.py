89c2c2a66507bae62954dccf80a22efadc874ffb
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-10-01 22:44:35 +0530

    52479

--- account/urls.py
+++ account/urls.py
@@ -1,18 +1,6 @@
-from rest_framework import serializers
-from .models import Table
-class TableSerializer(serializers.Modelserializer):
-    class Meta:
-        model = Table
-        fields = ['table_number', 'capacity', 'is_available']
-from rest_framework import generics
-from .models import Table
-from .serializers import TableSerializer
-class AvailableTableAPIView(generics.ListAPIView):
-    serializer_class = TableSerializer
-    def get_queryset(self):
-        return Table.objects.filter(is_available=True)
-        from django.urls import path
-        from .views import AvailableTableAPIView
-  urlpatterns = [
-    path('api/tables/available/', AvailableTableAPIView.as_view(), name='available_tables_api'),
-    ]      
+from datetime import date
+from django.db.models import Sum
+from .models import Order
+def get_daily_sales_total(date: date) -> float:
+    total = Order.objects.filter(created_at_date=date).aggregate(total_sum=Sum('toal_price'))['total_sum'] or 0
+    return total    
