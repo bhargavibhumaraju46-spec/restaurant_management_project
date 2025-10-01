6303a61175e2873c5c887280fe9820f099775ee3
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-10-01 22:37:09 +0530

    52482

--- account/urls.py
+++ account/urls.py
@@ -1,23 +1,18 @@
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-de send_email(recipient_email, subject, message_body):
-try:
-    smtp_server = "smtp.gmail.com"
-    port = 587
-    sender_email = "your_email@gmail.com"
-    password = "your_password"
-    msg = MIMEMultipart()
-    msg['From'] = sender_email
-    msg['To'] = recipient_email
-    msg['Subject'] = subject
-    msg.attach(MIMEText(message_body, 'plain'))
-    server = smtplib.SMTP(smtp_server, port)
-    server.starttls()
-    server.login(sender_email, password)
-    text = msg.as_string()
-    server.sendmail(sender_email, recipient_email, text)
-    server.quit()
-    print("Email sent successfully!")
-    except Exception as e:
-        print(f"Error sending email: {e}")
+from rest_framework import serializers
+from .models import Table
+class TableSerializer(serializers.Modelserializer):
+    class Meta:
+        model = Table
+        fields = ['table_number', 'capacity', 'is_available']
+from rest_framework import generics
+from .models import Table
+from .serializers import TableSerializer
+class AvailableTableAPIView(generics.ListAPIView):
+    serializer_class = TableSerializer
+    def get_queryset(self):
+        return Table.objects.filter(is_available=True)
+        from django.urls import path
+        from .views import AvailableTableAPIView
+  urlpatterns = [
+    path('api/tables/available/', AvailableTableAPIView.as_view(), name='available_tables_api'),
+    ]      
