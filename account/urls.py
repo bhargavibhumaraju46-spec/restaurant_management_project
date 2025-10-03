86549d540b882ea19e864872d94a561d995d1f66
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-10-03 21:23:14 +0530

    53030

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,3 @@
-#orders/models.py
+#home/models.py
 from django.db import models
-class ordermanager(models.mamager):
-    def pending_orders(self):
-        return self.filter(status='pending')
-        def shipped_orders(self):
-            return self.filter(status='shipped')
-            def by_status(self,status):
-                return self.filter(status=status)
+class menuitem(model.model): 
