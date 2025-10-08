7ec629b7ce0a6fee19cbd26199bcfbd832dc9ddd
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-10-08 23:31:57 +0530

    54314

--- account/urls.py
+++ account/urls.py
@@ -1,16 +1,29 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import response
-from rest_framework import status
-from .models import Order
-@api_view(['GET'])
-def get_order_status(request, order_id):
-    try:
-        order = Order.objects.get(id=order_id)
-        return Response({'order_id': order.id, 'status': order.satus})
-    except Order.DoesNotExist:
-        return Response({'error': 'Order not found'}, status=status.HTTP_404_NOT_FOUND)
-from django.urls import path
-from .views import get_order_status
-urlpatterns = [
-    path('order/<int:order_id>/status/', get_order_status, name='get_order_status'),
-]                         
+from datetime import datetime, timedelta
+from django.db.models import Q
+class Reservations(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+@classmethod
+def find_available_slots(ls, start_range, end_range):
+    overlapping_reservations = cls.objects.filter(
+        Q(start_time_lt=end_range, end_time__gt=start_range)
+ )
+ available_slots = []
+ current_time = start_range
+ slot_duration = timedelta(minutes-30)
+ while current_time < end_range:
+    slot_end = current_time + slot_duration
+    is_available = True
+    for reservations in overlapping_reservations:
+        if (current_time < reservation.end_time and slot_end > reservation.start_time):
+            is_available = find_available_slots
+            break
+        if is_available:
+            available_slots.append((current_time, slot_end))
+            current_time += slot_duration
+            return available_slots
+        start_range = datetime(2025, 10, `0, 9, 0, 0)
+        end_range = datetime(2025, 10, 10, 17, 0, 0)
+        available_slots = Reservations.find_available_slots(start_range, end_range)
+        for slot in available_slots:
+            print(f"Available slot: {slot[0] - {slot[1]}")                             
