51698f3d406dee1baa039268d8d6a196980fc136
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-10-12 22:15:25 +0530

    54655

--- account/apps.py
+++ account/apps.py
@@ -1,6 +1,25 @@
-from django.apps import AppConfig
 
+from django.db import models
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100)
+        def __str__(self):
+    from rest_framework import serializers
+    from .models import MenuCategory
+    
+    class MenuCategorySerializer(serializers.ModelSerializer):
+        class Meta:
+                model = MenuCategory
+                        fields = ['id', 'name']            return self.name
+                        from rest_framework.generics import ListAPIView
+                        from .models import MenuCategory
+                        from .serializers import MenuCategorySerializer
 
-class AccountConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'account'
+                        class MenuCategoryListAPIView(ListAPIView):
+                            queryset = MenuCategory.objects.all()
+                                serializer_class = MenuCategorySerializer
+                                from django.urls import path
+                                from .views import MenuCategoryListAPIView
+
+                                urlpatterns = [
+                                    path('menu-categories/', MenuCategoryListAPIView.as_view(), name='menu-category-list'),
+                                    ]
