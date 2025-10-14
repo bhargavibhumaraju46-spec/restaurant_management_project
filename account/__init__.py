08ff7d6889d84b6bbd22e46c2e0ca2ba19ead7ca
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-10-14 22:05:05 +0530

    55249

--- account/__init__.py
+++ account/__init__.py
@@ -1,35 +1,18 @@
-from rest_framework import serializers
-from .models import RestaurantOpeningHour(models.Model):
-class RestaurantOpeningHour(models.Model):
-    DAY_CHOICES = [
-        ('Monday', 'Monday'),
-        ('Tuesday', 'Tuesday'),
-        ('Wednesday', 'Wednesday'),
-        ('Thursday', 'Thursday'),
-        ('Friday', 'Friday'),
-        ('Saturday', 'Saturday'),
-        ('Sunday', 'Sunday'),
-    ]
-day = models.CharField(max_length=10, choices=DAY_CHOICES)
-opening_time = models.TimeField()
-closing_time = models.TimeField()
-from rest_framework import serializers
-from .models import RestaurantOpeningHour
-class RestaurantOpeningHourSerializer(serializers.Modelserializer):
-    class Meta:
-        model = RestaurantOpeningHour
-        fields = ['day', 'opening_time', 'closing_time']
+from django.db import models
+class Restaurant(models.Model):
+    opening_hours = models.CharField(max_length=50, dafault="11:00 AM - 11:00 PM (EST)")
 from rest_framework.response import response
-from rest_framework.views import APIView 
-from .models import RestaurantOpeningHour
-from .serializers import RestaurantOpeningHourSerializer
-class RestaurantOpeningHoursView(APIView):
+from rest_framework.views import APIView
+from .models import Restaurant
+class RestaurantHourView(APIView):
     def get(self, request):
-        opening_hours = RestaurantOpeningHour.objects.all()
-        serializer = RestaurantOpeningHourSerializer(opening_hours, many=True)
-     return Response(serializer.data)
- from django.urls import path
- from .views import RestaurantOpeningHoursView
- urlpatterns = [
-    path('api/restaurant/opening-hours/', RestaurantOpeningHoursView.as_view(), name='restaurant_opening_hours'),
- ]              
+        restaurant = Restaurant.objects.first()
+        if restaurant:
+            return Response({"opening_hours": restaurant.opening_hours})
+        else:
+            return Response({"error": "No restaurant found"},status=404)
+from django.urls import path
+from .views import RestaurantHourView
+urlpatterns = [
+    path('restaurant/hours/', RestaurantHourView.as_view(), name='restaurant-hours'),
+]                
