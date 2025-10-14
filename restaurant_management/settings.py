6a28e93dd1db5dc840ca63d55f7db5d648cfeec6
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-10-14 21:32:39 +0530

    54839

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -124,4 +124,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-autoy
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
