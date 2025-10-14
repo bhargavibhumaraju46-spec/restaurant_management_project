bcee6a68385fe387b43ab4f9c57c34bb8836f8de
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-10-14 20:49:48 +0530

    54843

--- account/urls.py
+++ account/urls.py
@@ -1,29 +1,24 @@
-from datetime import datetime, timedelta
-from django.db.models import Q
-class Reservations(models.Model):
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-@classmethod
-def find_available_slots(ls, start_range, end_range):
-    overlapping_reservations = cls.objects.filter(
-        Q(start_time_lt=end_range, end_time__gt=start_range)
- )
- available_slots = []
- current_time = start_range
- slot_duration = timedelta(minutes-30)
- while current_time < end_range:
-    slot_end = current_time + slot_duration
-    is_available = True
-    for reservations in overlapping_reservations:
-        if (current_time < reservation.end_time and slot_end > reservation.start_time):
-            is_available = find_available_slots
-            break
-        if is_available:
-            available_slots.append((current_time, slot_end))
-            current_time += slot_duration
-            return available_slots
-        start_range = datetime(2025, 10, `0, 9, 0, 0)
-        end_range = datetime(2025, 10, 10, 17, 0, 0)
-        available_slots = Reservations.find_available_slots(start_range, end_range)
-        for slot in available_slots:
-            print(f"Available slot: {slot[0] - {slot[1]}")                             
+from django.db import models
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    image_url = models.URLField(max_length=255)
+    from rest_framework import serializers
+    from .models import MenuItemclass MenuItemSerializer(serializers.ModelSerializer):
+       class Meta:
+        model = MenuItemfields = ['name', 'image_url']
+from rest_framework.response import response
+from rest_framework.views import APIView 
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+from django.db.modls import Q
+class MenuItemSearchView(APIView):
+    def get(self, request):
+        query = request.GET.get('q', '')
+        menu_items = MenuItem.objects.filter(name_icontains=query)
+        serializer = MenuItemSerializer(menu_items, many=True)
+        return Response(serializer.data)
+from django.urls import path
+from .views import MenuItemSearchView
+urlpatterns = [
+    path('api/menu/search/', MenuItemSearchView.as_view(), name'menu_search'),
+]                
