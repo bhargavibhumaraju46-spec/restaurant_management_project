3e7cddb56b76ffb86bfd9ae6205fcdf16893936e
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-10-16 22:21:55 +0530

    55492

--- account/__init__.py
+++ account/__init__.py
@@ -1,21 +1,21 @@
-def calculate_average_review_rating(reviews):
-    if not reviews:
-        return None
+from rest_framework.response import Response
+from response import status
+from rest_framework.views import APIView
+from .models import  MenuItem
+from .serializers import MenuItemAvailabilitySerializer
+class MenuItemAvailabilityView(APIView):
+    def get(self, request, item_id):
         try:
-            total_rating  = sum(review['rating'] for review in reviews)
-            average_rating = total_rating / len(reviews)
-            return float(average_rating)
-        except(KeyError, TypeError) as e:
-            print(f"Error calculating average rating: {e}")
-            return None
-         if __name__ == "__main__":
-            sample_reviews = [
-                {'rating': 4},
-                {'rating': 5},
-                {'rating': 6}
-            ]                                          
-            average_rating = calculate_average_review_rating(sample_reviews)
-            print(f"Average ratng:{average_rating}")
-            empty_reviews = []
-            average_rating_empty = calculate_average_review_rating(empty_reviews)
-            print(f"Average rating (empty): {average_rating_empty}")
+            menu_item = MenuItem.objects.get(id=item_id)
+            serializer = MenuItemAvailabilitySerializer({'available': menu_item.is_available})
+            return Response(serializer.data)
+        except MenuItem.DoesNotExist:
+            return Response({'error': 'Menu item not found'}, status=status.HTTP_404_NOT_FOUND) 
+from rest_framework import serializers
+class MenuItemAvailabilitySerializer(serializers.Serializer):
+    available = serializers.BooleanField()
+from django.urls import path
+from .views import MenuItemAvailabilityView
+urlpatterns = [
+    path('menu-items/<int:item_id>/availibility/', MenuItemAvailabilityView.as_view(), name='menu-item-availibility'),
+]                  
