0f536164f0254cb7bc93f0226dca8bc3be920171
bhargavibhumaraju46-spec <bhargavibhumaraju46@gmail.com>
2025-10-16 21:02:32 +0530

    55496

--- account/__init__.py
+++ account/__init__.py
@@ -1,23 +1,9 @@
 from django.db import models
-class Restaurant(models.Model):
-    name = models.CharField(max_length=100)
-    opening_hours = models.CharField(max_length=200, default="11:00 AM - 11:00 PM (EST)")
-    def __str__(self):
-        return self.name
-from rest_framework import generics
-from rest_framework import serializer
-from .models import Restaurant
-class RestaurantSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Restaurant
-        fields = ['opening_hours']
-   class OpeningHoursView(generics.ListAPIView):
-    queryset = Restaurant.objects.all()
-    serializer_class = RestaurantSerializer
-    def get_queryset(self):
-        return Restaurant.objects.all()[:1]
-from django.urls import path
-from .views import OpeningHoursView
-urlpatterns = [
-    path('restaurant-hours/', OpeningHoursView.as_view(), name='restaurant-hours'),
-]                        
+class MenuItem(models.Model):
+    cuisine = models.CharField(max_length=255)
+    @classthod
+    def get_by_cuisine(cls, cuisine_type):
+        return cls.objects.filter(cuisine=cuisine_type)
+from yourapp.models import MenuItemmenu_items = MenuItem.get_by_cuisine('Indian')
+for item in menu_items:
+    print(item)                             
